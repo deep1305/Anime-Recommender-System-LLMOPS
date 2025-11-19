aae1732544b2201ac0860734686a933c867c250d
deep1305 <shahdeep1399@Gmail.com>
2025-11-19 18:00:10 -0500

    Fix: Import RetrievalQA from langchain_community

--- src/recommender.py
+++ src/recommender.py
@@ -1,4 +1,4 @@
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community.chains import RetrievalQA
 from langchain_groq import ChatGroq
 from src.prompt_template import get_anime_prompt
 
